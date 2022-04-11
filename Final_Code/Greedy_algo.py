9c659a3a65aa0c63416cc058b554bef05ea7dd2f
MartaMastropietro <martamastro17@gmail.com>
2022-04-11 12:13:26 +0200

    greedy updated

--- Final_Code/Greedy_algo.py
+++ Final_Code/Greedy_algo.py
@@ -1,26 +1,31 @@
+import numpy as np
+
 from Classes import *
 
 class greedy:
     def __init__(self, env: Hyperparameters, current_profit, initial_prices):
         self.current_profit = current_profit
         self.check_convergence = False
-        self.best_prices = initial_prices
+        self.prices = initial_prices
         self.env=env
 
     def step(self):
-        greedy_profits= [0,0,0,0,0]
-        prices = self.best_prices
-        for i in range(5):
-            new_prices = prices
+        greedy_profits= np.zeros(5)
+        for i in range(5): #for every product we raise one price at a time
+            new_prices = self.prices
             if new_prices[i] != 3:
                 new_prices[i] += 1
                 day=Day(self.env, new_prices)
                 day.run_simulation()
                 greedy_profits[i] = day.profit
+            else:
+                self.check_convergence = True
+                print("Convergence of greedy reached")
 
-        if(np.max(greedy_profits) > self.current_profit):
-            best_product = np.argmax(greedy_profits)
-            self.best_prices[best_product] +=1
+        greedy_delta = np.diff(greedy_profits, np.ones(5) * self.current_profit)
+        if(np.max(greedy_delta) > 0 & self.check_convergence==False):
+            best_product = np.argmax(greedy_delta)
+            self.prices[best_product] +=1
         else:
             self.check_convergence = True
             print("Convergence of greedy reached")
--- Final_Code/main.py
+++ Final_Code/main.py
@@ -2,7 +2,12 @@ from Classes import *
 from Greedy_algo import greedy
 
 #enviroment definition
-hyper = Hyperparameters(...)
+transition_prob_listofmatrix=...
+dir_params_listofvector=...
+pois_param_vector=...
+conversion_rate_listofmatrix=...
+margin_matr=...
+hyper = Hyperparameters(transition_prob_listofmatrix, dir_params_listofvector, pois_param_vector, conversion_rate_listofmatrix, margin_matr)
 
 
 #testing greedy
@@ -11,7 +16,8 @@ starting_day = Day(hyper,starting_prices)
 starting_day.run_simulation()
 
 g=greedy(hyper, starting_day.profit , starting_prices)
-
 while (g.check_convergence==False) :
           g.step()
-print(g.best_prices)
+
+greedy_best_prices=g.prices
+print("Best prices indexes by greedy algorithm:",greedy_best_prices)

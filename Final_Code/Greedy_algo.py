3c40d6a6676b7ac84bdda9733a3dd48e22116a58
MartaMastropietro <martamastro17@gmail.com>
2022-04-11 14:46:49 +0200

    greedy updated

--- Final_Code/Greedy_algo.py
+++ Final_Code/Greedy_algo.py
@@ -1,10 +1,10 @@
 import numpy as np
-
+import copy
 from Classes import *
 
 class greedy:
-    def __init__(self, env: Hyperparameters, current_profit, initial_prices):
-        self.current_profit = current_profit
+    def __init__(self, env: Hyperparameters, init_profit, initial_prices):
+        self.current_profit = init_profit
         self.check_convergence = False
         self.prices = initial_prices
         self.env=env
@@ -12,23 +12,23 @@ class greedy:
     def step(self):
         greedy_profits= np.zeros(5)
         for i in range(5): #for every product we raise one price at a time
-            new_prices = self.prices
+            new_prices = copy.deepcopy(self.prices)
             if new_prices[i] != 3:
                 new_prices[i] += 1
                 day=Day(self.env, new_prices)
                 day.run_simulation()
                 greedy_profits[i] = day.profit
             else:
-                self.check_convergence = True
-                print("Convergence of greedy reached")
+                greedy_profits[i]=self.current_profit
 
         greedy_delta = np.diff(greedy_profits, np.ones(5) * self.current_profit)
-        if(np.max(greedy_delta) > 0 & self.check_convergence==False):
+        if(np.max(greedy_delta) > 0):
             best_product = np.argmax(greedy_delta)
             self.prices[best_product] +=1
+            self.current_profit=greedy_profits[best_product]
         else:
             self.check_convergence = True
             print("Convergence of greedy reached")
 
-
+# QUESTION: should we use number of users -> normalized profits?
 

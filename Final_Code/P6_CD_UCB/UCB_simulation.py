c6dab69b17fdf8d82d4742ce06b7144e1127fbcd
JacopoGhirri <jacopo.ghirri@gmail.com>
2022-04-25 14:37:27 +0200

    Clairvoyant dynamic

--- Final_Code/P6_CD_UCB/UCB_simulation.py
+++ Final_Code/P6_CD_UCB/UCB_simulation.py
@@ -20,14 +20,20 @@ day_profit_per_prod = []
 day_prices = np.zeros(5, dtype=int)
 learner = Items_UCB_Learner(env)
 
-#best_prices = np.zeros(5, dtype=int)
-#for i in range(5):
-#    best_prices[i] = np.argmax(env.global_margin[i, :]*(env.global_conversion_rate[0][i, :]*env.pois_param[0] +
-#                                                        env.global_conversion_rate[1][i, :]*env.pois_param[1] +
-#                                                        env.global_conversion_rate[2][i, :]*env.pois_param[2]))
-#best_prices = np.array(best_prices, dtype=int)
-#cl_profit = []
+best_prices = [np.zeros(5, dtype=int) for i in range(len(env.phases)+1)]
+
+for p in range(len(env.phases)+1):
+    for i in range(5):
+        best_prices[p][i] = np.argmax(env.global_margin[i, :]*(env.global_conversion_rate[p][0][i, :]*env.pois_param[0] +
+                                                               env.global_conversion_rate[p][1][i, :]*env.pois_param[1] +
+                                                               env.global_conversion_rate[p][2][i, :]*env.pois_param[2]))
+cl_profit = []
 sys.stdout.write('\r' + str("Beginning simulation") + '\n')
+
+phase_big = [-1]
+for i in range(len(env.phases)):
+    phase_big.append(env.phases[i])
+phase_big.append(np.inf)
 
 for t in range(time_horizon):
     sys.stdout.write('\r' + "Simulation progress: " + f'{t * 100 / time_horizon} %')
@@ -38,8 +44,14 @@ for t in range(time_horizon):
     learner.update(day)
     day_prices = learner.pull_prices()
 
-#    cl_profit.append(day.run_clairvoyant_simulation(best_prices))
-    env.t+=1
+    index = -1
+
+    for i in range(len(env.phases) + 1):
+        if phase_big[i] < env.t <= phase_big[i + 1]:
+            index = i
+
+    cl_profit.append(day.run_clairvoyant_simulation(best_prices[index]))
+    env.t += 1
 
 sys.stdout.flush()
 sys.stdout.write('\r' + str("Simulation completed under time horizon t = ") + str(time_horizon) + str(" days") + '\n')
@@ -48,8 +60,8 @@ final_prices = day_prices
 print(f'Final price configuration: {final_prices}')
 plt.figure(0)
 plt.plot(day_profit, color='red')
-#plt.plot(cl_profit, color='blue')
-#plt.legend(["UCB", "Optimal"], loc='best')
+plt.plot(cl_profit, color='blue')
+plt.legend(["UCB", "Optimal"], loc='best')
 plt.title("Profit - simulation")
 plt.xlabel("time [day]")
 plt.ylabel("profit [euros]")

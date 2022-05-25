d6d060d478752cbb3f23c0b4c0fb7a26376b1fa4
JacopoGhirri <jacopo.ghirri@gmail.com>
2022-05-25 10:16:43 +0200

    better profit puller

--- Final_Code/P1_Base/data_base.py
+++ Final_Code/P1_Base/data_base.py
@@ -5,7 +5,7 @@ matrix = np.array([[0.,   0.5,   0.5,   0.,    0.],
                    [0.,   0.,    0.,    0.5,   0.5],
                    [0.5,  0.,    0.,    0.,    0.5],
                    [0.5,  0.5,   0.,    0.,    0.]])
-transition_prob_listofmatrix = [matrix for i in range(3)]
+transition_prob_listofmatrix = [matrix/(i+1)**2 for i in range(3)]
 vec = 100*np.ones(6)
 dir_params_listofvector = [vec for i in range(3)]
 pois_param_vector = [1000 for i in range(3)]
@@ -15,7 +15,7 @@ mat = np.array([[0.1,   0.2,  0.3,  0.2],
                 [0.5,   0.1,  0.1,  0.1],
                 [0.1,   0.3,  0.1,    0]])
 
-conversion_rate_listofmatrix = [mat for i in range(3)]
+conversion_rate_listofmatrix = [mat/(i+1)**2 for i in range(3)]
 margin_matrix = np.array([[10, 15, 20, 25],
                           [30, 35, 40, 45],
                           [20, 25, 30, 35],

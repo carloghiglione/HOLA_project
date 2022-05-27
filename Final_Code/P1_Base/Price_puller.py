d0e24e450c887db431f7edbcadb6fe3d9e3ee63c
JacopoGhirri <jacopo.ghirri@gmail.com>
2022-05-27 18:06:36 +0200

    explorer

--- Final_Code/P1_Base/Price_puller.py
+++ Final_Code/P1_Base/Price_puller.py
@@ -78,11 +78,11 @@ def profit_puller(prices, conv_rate_full, margins_full, tran_prob, alphas, mepp,
         pur_prob[2, :] += prob_per_p1[2, :] * (alphas[2][p1 + 1])
 
     profit = 0
-    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[0])))*pois[0]
-    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[1])))*pois[1]
-    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[2])))*pois[2]
+    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[0])))*float(pois[0])
+    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[1])))*float(pois[1])
+    profit += float(np.sum(pur_prob*margin*(1.0 + mepp[2])))*float(pois[2])
 
-    return profit/np.sum(pois)
+    return profit/float(np.sum(pois))
 
 
 def pull_prices(env, conv_rates, alpha, n_buy, trans_prob, print_message="Simulating") -> np.array:
@@ -126,7 +126,7 @@ def pull_prices(env, conv_rates, alpha, n_buy, trans_prob, print_message="Simula
     count = 0
     cc = 4**5
     prices = -1*np.ones(shape=(cc, 5), dtype=int)
-    profits = np.zeros(cc, dtype=int)
+    profits = np.zeros(cc, dtype=float)
 
     sim_prices = np.zeros(5, dtype=int)
 
@@ -153,7 +153,6 @@ def pull_prices(env, conv_rates, alpha, n_buy, trans_prob, print_message="Simula
                         count += 1
                 sys.stdout.write('\r' + print_message + str(", pulling prices: ") + f'{count * 100 / cc} %')
     sys.stdout.write('\r' + print_message + str(", pulling prices: 100%"))
-    profits = np.array(profits, dtype=float)
     best = np.argmax(profits)
     return prices[best]
 
@@ -327,7 +326,7 @@ def pull_prices_explor(env, conv_rates, alpha, n_buy, trans_prob, print_message=
     count = 0
     cc = 4**5
     prices = -1*np.ones(shape=(cc, 5), dtype=int)
-    profits = np.zeros(cc, dtype=int)
+    profits = np.zeros(cc, dtype=float)
 
     sim_prices = np.zeros(5, dtype=int)
 
@@ -354,6 +353,4 @@ def pull_prices_explor(env, conv_rates, alpha, n_buy, trans_prob, print_message=
                         count += 1
                 sys.stdout.write('\r' + print_message + str(", pulling prices: ") + f'{count * 100 / cc} %')
     sys.stdout.write('\r' + print_message + str(", pulling prices: 100%"))
-    profits = np.array(profits, dtype=float)
-    best = np.argmax(profits)
     return profits
